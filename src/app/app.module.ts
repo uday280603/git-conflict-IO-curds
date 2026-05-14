62633d14a52098a90b0a44dda0ccef8d64d3b0c8
uday <udayindrawar123@gmail.com>
2026-05-14 18:56:48 +0530

    component Created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TodoDashpboardComponent } from './shared/component/todo-dashpboard/todo-dashpboard.component';
+import { TodoDashboardComponent } from './shared/component/todo-dashboard/todo-dashboard.component';
+import { TodoListComponent } from './shared/component/todo-list/todo-list.component';
+import { TodoFormComponent } from './shared/component/todo-form/todo-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoDashpboardComponent,
+    TodoDashboardComponent,
+    TodoListComponent,
+    TodoFormComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/shared/component/todo-dashboard/todo-dashboard.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Itodo } from '../../models/Itodo';
+import { todoData } from '../../const/Itodo';
+
+@Component({
+  selector: 'app-todo-dashboard',
+  templateUrl: './todo-dashboard.component.html',
+  styleUrls: ['./todo-dashboard.component.scss']
+})
+export class TodoDashboardComponent implements OnInit {
+
+  todoArr !: Itodo[];
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+    this.todoArr = todoData
+
+
+  }
+
+}
