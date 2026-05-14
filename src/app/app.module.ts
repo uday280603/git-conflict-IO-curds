67053414762e372294a07e4a13cf8e6c75f33c8e
uday <udayindrawar123@gmail.com>
2026-05-14 19:13:14 +0530

    installed bootstrap and Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-import { TodoDashpboardComponent } from './shared/component/todo-dashpboard/todo-dashpboard.component';
 import { TodoDashboardComponent } from './shared/component/todo-dashboard/todo-dashboard.component';
 import { TodoListComponent } from './shared/component/todo-list/todo-list.component';
 import { TodoFormComponent } from './shared/component/todo-form/todo-form.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodoDashpboardComponent,
     TodoDashboardComponent,
     TodoListComponent,
     TodoFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
